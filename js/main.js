507db0743ecf609e3052983fdfa3fdfbe9521558
Ahmed Ramzy <46076630+AhmedRamzyy@users.noreply.github.com>
2019-07-27 20:20:42 +0200

    some if condetions on input values

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 var numOne = document.getElementById("num_one");
 var numTwo = document.getElementById("num_two");
 var answer = document.getElementById("result");
@@ -8,27 +7,44 @@ var subtractButton = document.getElementById("subtract");
 var divideButton = document.getElementById("divide");
 var multiplyButton = document.getElementById("multiply");
 
-function add(){
-    var one = parseFloat(numOne.value) || 0 ;
-    var two = parseFloat(numTwo.value) || 0  ;
-
-    answer.innerHTML = "Answer: " + (one+two);
+function add() {
+    var one = parseFloat(numOne.value) ;
+    var two = parseFloat(numTwo.value) ;
+    if (isNaN(one) === true || isNaN(two) === true ) {
+        answer.innerHTML = "You Must Enter A Number " ;
+    }else{
+        answer.innerHTML = "Answer: " + (one + two);
+    }
 }
-function subtract(){
-    var one = parseFloat(numOne.value) || 0  ;
-    var two = parseFloat(numTwo.value) || 0  ;
 
-    answer.innerHTML = "Answer: " + (one-two);
+function subtract() {
+    var one = parseFloat(numOne.value) ;
+    var two = parseFloat(numTwo.value) ;
+    if (isNaN(one) === true || isNaN(two) === true ) {
+        answer.innerHTML = "You Must Enter A Number " ;
+    }else{
+        answer.innerHTML = "Answer: " + (one - two);
+    }
 }
-function multiply(){
-    var one = parseFloat(numOne.value) || 0  ;
-    var two = parseFloat(numTwo.value) || 0  ;
 
-    answer.innerHTML = "Answer: " + (one*two);
+function multiply() {
+    var one = parseFloat(numOne.value) ;
+    var two = parseFloat(numTwo.value) ;
+    if (isNaN(one) === true || isNaN(two) === true ) {
+        answer.innerHTML = "You Must Enter A Number " ;
+    }else{
+        answer.innerHTML = "Answer: " + (one * two);
+    }
 }
-function divide(){
-    var one = parseFloat(numOne.value) || 0  ;
-    var two = parseFloat(numTwo.value) || 0  ;
 
-    answer.innerHTML = "Answer: " + (one/two);
+function divide() {
+    var one = parseFloat(numOne.value) ;
+    var two = parseFloat(numTwo.value) ;
+    if (isNaN(one) === true || isNaN(two) === true )  {
+        answer.innerHTML = "You Must Enter A Number " ;
+    }else if(two === 0){
+        answer.innerHTML = "Cannot Divide By Zero" ;
+    }else{
+        answer.innerHTML = "Answer: " + (one / two);
+    }
 }
