4104b775acbcb0d8d02ea62e42d6c9a9db7a295d
Ahmed Ramzy <46076630+AhmedRamzyy@users.noreply.github.com>
2019-07-27 17:54:23 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+var numOne = document.getElementById("num_one");
+var numTwo = document.getElementById("num_two");
+var answer = document.getElementById("result");
+
+var addButton = document.getElementById("add");
+var subtractButton = document.getElementById("subtract");
+var divideButton = document.getElementById("divide");
+var multiplyButton = document.getElementById("multiply");
+
+addButton.addEventListener("click", add);
+subtractButton.addEventListener("click", subtract);
+divideButton.addEventListener("click", divide);
+multiplyButton.addEventListener("click", multiply);
+
+function add(){
+    var one = parseFloat(numOne.value) || 0 ;
+    var two = parseFloat(numTwo.value) || 0  ;
+
+    answer.innerHTML = "Answer: " + (one+two);
+}
+function subtract(){
+    var one = parseFloat(numOne.value) || 0  ;
+    var two = parseFloat(numTwo.value) || 0  ;
+
+    answer.innerHTML = "Answer: " + (one-two);
+}
+function divide(){
+    var one = parseFloat(numOne.value) || 0  ;
+    var two = parseFloat(numTwo.value) || 0  ;
+
+    answer.innerHTML = "Answer: " + (one/two);
+}
+function multiply(){
+    var one = parseFloat(numOne.value) || 0  ;
+    var two = parseFloat(numTwo.value) || 0  ;
+
+    answer.innerHTML = "Answer: " + (one*two);
+}
+
